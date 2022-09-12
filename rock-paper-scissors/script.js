32ff92313536fa8f6083dd5524d2703882dc5ccc
Hershell Koger <hersheykoger@yahoo.com>
2022-09-12 04:06:42 -0400

    Add eventlistener to buttons to inject button choice into myChoice span element

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -2,3 +2,10 @@ const computerChoice = document.getElementById("computer-choice");
 const myChoice = document.getElementById("my-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
+
+let userTally;
+
+possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
+    userTally = e.target.id;
+    myChoice.innerHTML = userTally;
+}))
