d1041f955a3ed6fe841238c79f79deb6a1f1742d
Hershell Koger <hersheykoger@yahoo.com>
2022-09-12 05:00:18 -0400

    Create getResult function - it uses a series if/else statments to determine who won, the user or computer

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -5,6 +5,7 @@ const possibleChoices = document.querySelectorAll("button");
 
 let userTally;
 let computerTally;
+let result;
 
 function generateRandom() {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length);
@@ -12,8 +13,8 @@ function generateRandom() {
     if (randomNumber === 0) computerTally = "rock";
     else if (randomNumber === 1) computerTally = "paper";
     else computerTally = "scissors";
-
     computerChoice.innerHTML = computerTally;
+    getResult();
 }
 
 possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
@@ -22,3 +23,15 @@ possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
     generateRandom();
 
 }))
+
+function getResult() {
+    if(computerTally === userTally) result = "It's a draw"
+    else if(computerTally === 'rock' && userTally === 'paper')  result = "You win"
+    else if(computerTally === 'rock' && userTally === 'scissors')  result = "You lost"
+    else if(computerTally === 'paper' && userTally === 'rock')  result = "You lost"
+    else if(computerTally === 'paper' && userTally === 'scissors')  result = "You win"
+    else if(computerTally === 'scissors' && userTally === 'paper')  result = "You lost"
+    else if(computerTally === 'scissors' && userTally === 'rock')  result = "You win"
+
+    resultDisplay.innerHTML = result;
+}
