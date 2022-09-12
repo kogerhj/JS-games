f3cb3c7d9a3214b8f0d2a4129898962f085f0042
Hershell Koger <hersheykoger@yahoo.com>
2022-09-12 03:59:20 -0400

    Create binding that selects all buttons

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,3 +1,4 @@
 const computerChoice = document.getElementById("computer-choice");
 const myChoice = document.getElementById("my-choice");
 const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
