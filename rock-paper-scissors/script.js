f2316da7ead2b0f64a451ff765a50d6b9959fd8b
Hershell Koger <hersheykoger@yahoo.com>
2022-09-12 04:41:06 -0400

    Add if/else statments in gernerateNumber() to generate the computer's choice - rock, paper, or scissors

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -4,14 +4,21 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 
 let userTally;
+let computerTally;
 
 function generateRandom () {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length);
-    console.log(randomNumber);
+
+    if (randomNumber === 0) computerTally = "rock";
+    else if (randomNumber === 1) computerTally = "paper";
+    else computerTally = "scissors";
+
 }
 
 possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
     userTally = e.target.id;
     myChoice.innerHTML = userTally;
     generateRandom();
+
+   
 }))
