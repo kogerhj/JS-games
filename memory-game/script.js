4e5a7e580a3ad830a5758939c590a9ca76fc46f6
Hershell Koger <hersheykoger@yahoo.com>
2022-09-13 09:35:49 -0400

    Create an array of objects - each object contains name and img properties with respective name and image values

---
+++ memory-game/script.js
@@ -0,0 +1,41 @@
+const imageArray = [
+    {
+        name: 'apple',
+        img: 'images/apple.svg',
+    },
+
+    {
+        name: 'banana',
+        img: 'images/banana.svg',
+    },
+
+    {
+        name: 'cantaloupe',
+        img: 'images/cantaloupe.svg',
+    },
+
+    {
+        name: 'citron-citrus',
+        img: 'images/citron-citrus.svg',
+    },
+
+    {
+        name: 'pear',
+        img: 'images/pear.svg',
+    },
+
+    {
+        name: 'plum',
+        img: 'images/plum.svg',
+    },
+
+    {
+        name: 'question',
+        img: 'images/question.svg',
+    },
+
+    {
+        name: 'white',
+        img: 'images/white.svg',
+    },
+]
