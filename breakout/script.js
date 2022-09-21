5f7a38b9ee2bf95c01c9884cfa6d09e230549898
Hershell Koger <hersheykoger@yahoo.com>
2022-09-21 12:09:25 -0400

    Remove spacing

--- breakout/script.js
+++ breakout/script.js
@@ -53,13 +53,8 @@ function addBlocks() {
 addBlocks();
 
 //add user
-
 const user = document.createElement('div');
-
 user.classList.add('user');
-
 user.style.left = currentPosition[0] + "px";
-
 user.style.bottom = currentPosition[1] + "px";
-
 grid.appendChild(user);
