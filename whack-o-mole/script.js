630eb7190f8bf1bfd01e6d56502613cc722f790a
Hershell Koger <hersheykoger@yahoo.com>
2022-09-20 22:30:03 -0400

    Create randomSquare function - use forEach to cycle through the squares and remove 'mole'

--- whack-o-mole/script.js
+++ whack-o-mole/script.js
@@ -5,3 +5,11 @@ const myScore = document.querySelector("#score");
 const squares = document.querySelectorAll(".square");
 
 const mole = document/querySelector(".mole");
+
+let result = 0;
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole');
+    })
+}
