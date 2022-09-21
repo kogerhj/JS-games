befaca4791c301b115bd81c6651781888df63ce4
Hershell Koger <hersheykoger@yahoo.com>
2022-09-21 11:44:44 -0400

    Add total of 15 new Blocks to blocks array

--- breakout/script.js
+++ breakout/script.js
@@ -1,5 +1,3 @@
-const block = document.createElement('div');
-
 const grid = document.querySelector('.grid');
 
 const blockHeight = 20;
@@ -17,13 +15,29 @@ class Block {
 
 //all my blocks
 const blocks = [
-    new Block(10, 270)
+    new Block(10, 270),
+    new Block(130, 270),
+    new Block(250, 270),
+    new Block(370, 270),
+    new Block(490, 270),
+    new Block(10, 230),
+    new Block(130, 230),
+    new Block(250, 230),
+    new Block(370, 230),
+    new Block(490, 230),
+    new Block(10, 190),
+    new Block(130, 190),
+    new Block(250, 190),
+    new Block(370, 190),
+    new Block(490, 190),
+
 ]
 
 
 // add all blocks to grid
 function addBlocks() {
     for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div');
         block.classList.add('block')
         block.style.left = blocks[i].bottomLeft[0] + 'px'
         block.style.bottom = blocks[i].bottomLeft[1] + 'px'
