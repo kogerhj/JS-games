b2be1070ca182203be4a128be8624c527212a405
Hershell Koger <hersheykoger@yahoo.com>
2022-09-21 11:11:45 -0400

    Create class Block that takes an x and y axis point to create individual blocks - reference point for axes is bottom left of block

--- breakout/script.js
+++ breakout/script.js
@@ -2,9 +2,24 @@ const block = document.createElement('div');
 
 const grid = document.querySelector('.grid');
 
+const blockHeight = 20;
+const blockWidth = 100;
+
+//create Block
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
+        this.bottomRight = [xAxis + blockWidth, yAxis];
+        this.topLeft = [xAxis, yAxis + blockHeight];
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+    }
+}
+
+
+//add block to grid
 function addBlock() {
     block.classList.add('block');
-    block.style.left = '100x';
+    block.style.left = '100px';
     block.style.bottom = '50px';
     grid.appendChild(block);
 }
