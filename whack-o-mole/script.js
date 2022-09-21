1fb953219f97d53f1bc8f3ce1ead85d6b9715cfa
Hershell Koger <hersheykoger@yahoo.com>
2022-09-20 20:01:23 -0400

    Create bindings to target: time-left, score, squares, and mole

---
+++ whack-o-mole/script.js
@@ -0,0 +1,7 @@
+const timeLeft = document.querySelector("#time-left");
+
+const myScore = document.querySelector("#score");
+
+const squares = document.querySelectorAll(".square");
+
+const mole = document/querySelector(".mole");
