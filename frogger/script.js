b3a59f4a488df71e6f19775dabf2260cdb84fec6
Hershell Koger <hersheykoger@yahoo.com>
2022-09-27 07:47:51 -0400

    Add class 'from' to stylesheet; add eventListener that invokes newly created moveFrog() function

--- frogger/script.js
+++ frogger/script.js
@@ -2,4 +2,10 @@ const timeLeft = document.querySelector('#time-left');
 const result = document.querySelector('#result');
 const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll('.grid div');
+let currentIndex = 0
 
+function moveFrog() {
+   squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup', moveFrog)
