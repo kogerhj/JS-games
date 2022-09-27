4f76c686c54c53724bd7cb5f67448e12a06fe2a6
Hershell Koger <hersheykoger@yahoo.com>
2022-09-26 23:50:21 -0400

    Add bindings that select: time-left, result, start/pause button, squares

---
+++ frogger/script.js
@@ -0,0 +1,5 @@
+const timeLeft = document.querySelector('#time-left');
+const result = document.querySelector('#result');
+const startPauseButton = document.querySelector('#start-pause-button');
+const squares = document.querySelectorAll('.grid div');
+
